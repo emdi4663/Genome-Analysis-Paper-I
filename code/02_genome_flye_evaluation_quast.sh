2899507a5ac6a4738bdc0a441a41b50c00b24fe4
Emma Dizdarevic <emdi4663@s22.uppmax.uu.se>
2024-04-14 15:31:25 +0200

    Script of the Flye genome assembly evaluation by Quast

---
+++ code/02_genome_flye_evaluation_quast.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+#SBATCH-A uppmax2024-2-7
+#SBATCH-M snowy
+#SBATCH-p core
+#SBATCH-n 2
+#SBATCH-t 01:00:00
+#SBATCH-J flye_evaluation_quast
+#SBATCH--mail-type=ALL
+#SBATCH--mail-user emdi4663@student.uu.se
+#SBATCH--output=%x.%j.out
+
+#Load the modules
+module load bioinfo-tools
+module load quast/5.0.2
+
+#The variables
+
+flye_assembly=/home/emdi4663/Genome-Analysis-Paper-I/analyses/02_genome_assembly/pilon/*.fasta
+genome_ref=/home/emdi4663/Genome-Analysis-Paper-I/data/genome_reference/*.fna.gz
+output_path=/home/emdi4663/Genome-Analysis-Paper-I/analyses/02_genome_assembly/flye_evaluation_quast
+
+
+#The commands
+python /sw/bioinfo/quast/5.0.2/snowy/bin/quast.py \
+$flye_assembly \
+-r $genome_ref \
+-o $output_path
